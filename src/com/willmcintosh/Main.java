3697cc62df5cc22c27603b8fc90112542766ffff
WillMcIntosh <mcintoshwilliamh@gmail.com>
2018-03-19 17:03:12 -0400

    initial commit: created Applet

---
+++ src/com/willmcintosh/Main.java
@@ -0,0 +1,8 @@
+package com.willmcintosh;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
---
+++ src/com/willmcintosh/Pong.java
@@ -0,0 +1,21 @@
+package com.willmcintosh;
+
+import java.applet.Applet;
+import java.awt.*;
+
+public class Pong extends Applet {
+
+    public void init() {
+
+    }
+
+    public void paint(Graphics g) {
+        super.paint(g);
+    }
+
+    public void update(Graphics g) {
+
+    }
+
+
+}
